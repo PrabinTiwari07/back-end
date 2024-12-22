9d752ef40b547abe0b2d3c82333d70b4d1863e6d
ItachiPrabin <itachiprabin@gmail.com>
2024-12-22 23:25:15 +0545

    user model

---
+++ model/user.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+// User Schema
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, default: 'user' } // Example roles: 'user', 'admin'
+});
+
+// Pre-save middleware to hash passwords
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+module.exports = mongoose.model('User', userSchema);
