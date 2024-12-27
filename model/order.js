6fcf12eacda37440a3af9b241143bb7b730388c8
ItachiPrabin <itachiprabin@gmail.com>
2024-12-27 22:45:33 +0545

    order details

---
+++ model/order.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const OrderSchema = new mongoose.Schema({
+    customerName: {
+        type: String,
+        required: true
+    },
+    customerEmail: {
+        type: String,
+        required: true
+    },
+    services: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Service',
+        required: true
+    }],
+    totalAmount: {
+        type: Number,
+        required: true
+    },
+    orderDate: {
+        type: Date,
+        default: Date.now
+    },
+    // Add any other relevant fields, such as status
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Order', OrderSchema);
