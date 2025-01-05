6fc8ef5e75a2ba91f7e3e3ed3d3195f330b0f5de
ItachiPrabin <itachiprabin@gmail.com>
2025-01-05 22:23:44 +0545

    app.js modified

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ app.use(express.json());
 // Routes
 app.use("/api/users", require("./routes/UserRoute"));
 
+app.use("/api/orders", require("./routes/orderRoutes")); // Correctly importing order routes
+
+app.use("/api/services", require("./routes/serviceRoutes")); // Correctly importing order routes
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
