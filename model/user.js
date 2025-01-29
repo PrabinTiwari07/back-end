e987e9c7c82e708fa61e4c8f453c1943a96d7598
ItachiPrabin <itachiprabin@gmail.com>
2025-01-29 22:20:36 +0545

    OTP added

--- model/user.js
+++ model/user.js
@@ -1,4 +1,3 @@
-
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
@@ -7,9 +6,11 @@ const userSchema = new mongoose.Schema({
   phone: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  confirm_password: { type: String, required: true },
+  role: { type: String, default: "user" },
+  isVerified: { type: Boolean, default: false }, // Email verification status
+  otp: { type: String },
+  otpExpires: { type: Date },
 });
 
-module.exports = mongoose.model("User", userSchema);
-
-
+const User = mongoose.model("User", userSchema);
+module.exports = User;
