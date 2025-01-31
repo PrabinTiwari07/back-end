add7344fa4b73ace8d004766266ad7bdd87a502e
ItachiPrabin <itachiprabin@gmail.com>
2025-01-31 17:27:29 +0545

    added profile and OTP

--- model/user.js
+++ model/user.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
   fullname: { type: String, required: true },
   address: { type: String, required: true },
   phone: { type: String, required: true },
+  image : {type: String, required:true},
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, default: "user" },
